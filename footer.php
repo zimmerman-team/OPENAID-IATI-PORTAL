a87bb8ee21e563ecac0fbf22155c82275e119b58
Daan <daancjanssen@gmail.com>
2015-05-18 13:08:37 +0200

    CSS

--- footer.php
+++ footer.php
@@ -3,16 +3,17 @@
 		<footer>
 		  <div class="container">
 		  	<div class="row">
-		  		<div class="col-md-offset-3 col-md-6">
-		      		openaidNL Transparency Initiative.  Content licensed under a Creative Commons Attribution 3.0 Unported License
-		    	</div>
-		    </div>
-		    <div class="row">
 		      <img src="<?php echo get_template_directory_uri(); ?>/images/openaidnl-logo.png" />
 		    </div>
 		    <div class="row">
-		      Powered by: Zimmerman &amp; Zimmerman and Akvo
+		      Zimmerman &amp; Zimmerman
 		    </div>
+		  	<div class="row">
+		  		<h1>powered by:</h1>
+		  		Ministerie van Buitenlandse Zaken
+		    </div>
+		    
+		    
 		  </div>
 		</footer>
 
