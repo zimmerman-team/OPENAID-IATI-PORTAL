36df0d0de9d6085bf0076c8dd89bad92747e2b7c
Vincent van 't Westende <vincent@zimmermanzimmerman.nl>
2015-05-27 20:53:50 +0200

    update texts & logo

--- footer.php
+++ footer.php
@@ -5,13 +5,10 @@
 		  	<div class="row">
 		      <img src="<?php echo get_template_directory_uri(); ?>/images/openaidnl-logo.png" />
 		    </div>
-		    <div class="row">
-		      Zimmerman &amp; Zimmerman
-		    </div>
 		  	<div class="row">
 		  		<h1>Credits:</h1>
-		  		Designed & developed by: <a href="http://www.zimmermanzimmerman.nl">Zimmerman & Zimmerman</a><br>
-		  		Funded by: <a href="http://www.akvo.org">Akvo</a> as part of <a href="http://rsr.akvo.org/project/2210/">PPP Phase 2 2011-2014</a>
+		  		Designed & developed by: <a target="_blank" href="http://www.zimmermanzimmerman.nl">Zimmerman & Zimmerman</a><br>
+		  		Initiative and funding by: <a target="_blank" href="http://www.akvo.org">Akvo</a> as part of <a target="_blank" href="http://rsr.akvo.org/project/2210/">PPP Phase 2 2011-2014</a>
 		    </div>
 		    
 		    
