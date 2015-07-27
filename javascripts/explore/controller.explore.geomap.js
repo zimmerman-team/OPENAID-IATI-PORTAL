440b6666aa903e3ab5fde6b235f187ca7861e9c9
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2015-07-27 12:17:04 +0200

    small fixes

--- javascripts/explore/controller.explore.geomap.js
+++ javascripts/explore/controller.explore.geomap.js
@@ -30,7 +30,6 @@
       {'id':4, 'name': 'Handelsrelatie'}, 
       {'id':5, 'name': 'Overige'}];
 
-    vm.markerData = [];
     vm.defaults = {
       tileLayer: 'https://{s}.tiles.mapbox.com/v3/zimmerman2014.hmpkg505/{z}/{x}/{y}.png',
       maxZoom: 12,
@@ -58,6 +57,7 @@
     vm.selectionString = '';
 
     vm.geoView = 'landen';
+    vm.resultCounter = 0;
 
 
     /**
@@ -68,31 +68,46 @@
     function activate() {
 
       $scope.$watch("vm.selectedCountryRelation", function (selectionString) {
-          if(vm.markerData.length > 0){
-            vm.updateMarkers();
-          }
+          // if(vm.markerData.length > 0){
+          //   vm.updateMarkers();
+          // }
       }, true);
 
       $scope.$watch('vm.filterSelection.selectionString', function (selectionString) {
         vm.selectionString = selectionString;
-        Aggregations.aggregation('recipient-country-geo', 'iati-identifier', vm.selectionString).then(successFn, errorFn);
 
-        function successFn(data, status, headers, config) {
-
-          vm.markerData = data.data;
-          vm.updateMarkers();
+        for(var key in vm.markers){
+          vm.markers[key].opacity = 0;
         }
 
-        function errorFn(data, status, headers, config) {
-          console.log("getting countries failed");
-        }
+        Aggregations.aggregation('recipient-country-geo', 'iati-identifier', vm.selectionString).then(countrySuccessFn, errorFn);
+        Aggregations.aggregation('recipient-region-geo', 'iati-identifier', vm.selectionString).then(regionSuccessFn, errorFn);
+
+        
       }, true);
 
       $scope.$watch("vm.geoView", function (viewName) {
-          if(viewName == "regios"){
-          
+          for(var key in vm.markers){
+            vm.markers[key].opacity = 0;
           }
+
+          Aggregations.aggregation('recipient-country-geo', 'iati-identifier', vm.selectionString).then(countrySuccessFn, errorFn);
+          Aggregations.aggregation('recipient-region-geo', 'iati-identifier', vm.selectionString).then(regionSuccessFn, errorFn);
+
       }, true);
+
+
+      function countrySuccessFn(data, status, headers, config) {
+        vm.updateCountryMarkers(data.data);
+      }
+
+      function regionSuccessFn(data, status, headers, config){
+        vm.updateRegionMarkers(data.data);
+      }
+
+      function errorFn(data, status, headers, config) {
+        console.log("getting countries failed");
+      }
 
 
       // for each active country, get the results
@@ -107,32 +122,28 @@
       return vm.geoView == viewName ? true : false;
     }
 
-    vm.updateMarkers = function() {
+    vm.updateCountryMarkers = function(markerData) {
       
       var selectedCountryRelationMap = {};
       for(var i = 0;i < vm.selectedCountryRelation.length;i++){
         selectedCountryRelationMap[vm.selectedCountryRelation[i]['name'].replace(/\s/g, '')] = true;
       }
 
-      for(var key in vm.markers){
-        vm.markers[key].opacity = 0;
-      }
-
-      for (var i = 0; i < vm.markerData.length;i++){
+      for (var i = 0; i < markerData.length;i++){
          
         var partnerType = 'Overige';
-        if(partnerlanden[vm.markerData[i].country_id] !== undefined){
-          partnerType = partnerlanden[vm.markerData[i].country_id].replace(/\s/g, ''); 
+        if(partnerlanden[markerData[i].country_id] !== undefined){
+          partnerType = partnerlanden[markerData[i].country_id].replace(/\s/g, ''); 
         }
         
         if (selectedCountryRelationMap[partnerType] !== undefined){
 
-          if(vm.markers[vm.markerData[i].country_id] !== undefined){
-            vm.markers[vm.markerData[i].country_id].opacity = 1;
+          if(vm.markers[markerData[i].country_id] !== undefined){
+            vm.markers[markerData[i].country_id].opacity = 1;
           } else {
 
-            if(vm.markerData[i].location != null){
-              var location = vm.markerData[i].location.substr(6, (vm.markerData[i].location.length - 7));
+            if(markerData[i].location != null){
+              var location = markerData[i].location.substr(6, (markerData[i].location.length - 7));
               location = location.split(' ');
               var flag = vm.markerData[i].country_id;
               var flag_lc = flag.toLowerCase();
@@ -144,16 +155,46 @@
                 '<p><b>Activiteiten:</b> '+vm.markerData[i]['activity_count']+'</p>'+
                 '<p><b>Totaal budget:</b> XXX</p>'+
                 '<p><b>Type relatie:</b> '+partnerType+'</p>'+
-                '<a class="btn btn-default" href="'+homeUrl+'/landen/'+vm.markerData[i].country_id+'/">Ga naar overzicht land</a>',
+                '<a class="btn btn-default" href="'+homeUrl+'/landen/'+markerData[i].country_id+'/">Ga naar overzicht land</a>',
                 icon: vm.markerIcons[partnerType],
               }
             }
           }
         }
+
+        if(vm.geoView != 'landen' && vm.markers[markerData[i].country_id] !== undefined){
+          vm.markers[markerData[i].country_id].opacity = 0;
+        }
+      }
+    }
+
+    vm.updateRegionMarkers = function(markerData) {
+      
+      for (var i = 0; i < markerData.length;i++){
+
+        if(vm.markers[markerData[i].region_id] !== undefined){
+          vm.markers[markerData[i].region_id].opacity = 1;
+        } else if(markerData[i].location != null){
+          var location = markerData[i].location.substr(6, (markerData[i].location.length - 7));
+          location = location.split(' ');
+          vm.markers[markerData[i].region_id] = {
+            lat: parseInt(location[1]),
+            lng: parseInt(location[0]),
+            message: '<span class="flag-icon flag-icon-'+markerData[i].region_id+'"></span>'+
+            '<h4>'+markerData[i].name+'</h4>'+
+            '<p><b>Activiteiten:</b> '+markerData[i]['activity_count']+'</p>'+
+            '<p><b>Totaal budget:</b> XXX</p>'+
+            '<a class="btn btn-default" href="'+homeUrl+'/regios/'+markerData[i].region_id+'/">Ga naar regio overzicht</a>',
+            icon: vm.markerIcons['Overige'],
+          }
+        }
+
+        if(vm.geoView != 'regios' && vm.markers[markerData[i].region_id] !== undefined){
+          vm.markers[markerData[i].region_id].opacity = 0;
+        }
       }
     }
 
     activate();
-
   }
 })();
