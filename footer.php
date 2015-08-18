c3f0272390f8e4e1cfb3dae04d7a46d468fbf184
Daan <daan@zimmermanzimmerman.nl>
2015-08-18 16:59:38 +0200

    etc

--- footer.php
+++ footer.php
@@ -3,12 +3,16 @@
 		<footer>
 		  <div class="container">
 		  	<div class="row">
-		      <img src="<?php echo get_template_directory_uri(); ?>/images/openaidnl-logo.png" />
+		  		<div class="col-sm-6 col-md-4 col-lg-2 col-sm-offset-3 col-md-offset-4 col-lg-offset-5">
+				    <img src="<?php echo get_template_directory_uri(); ?>/images/openaidnl-logo.png" />
+			    </div>
 		    </div>
 		  	<div class="row">
-		  		<h1>Credits:</h1>
-		  		Designed & developed by: <a target="_blank" href="http://www.zimmermanzimmerman.nl">Zimmerman & Zimmerman</a><br>
-		  		Initiative and funding by: <a target="_blank" href="http://www.akvo.org">Akvo</a> as part of <a target="_blank" href="http://rsr.akvo.org/project/2210/">PPP Phase 2 2011-2014</a>
+		  		<div class="col-sm-12">
+			  		<h3>Credits:</h3>
+			  		Designed & developed by: <a target="_blank" href="http://www.zimmermanzimmerman.nl">Zimmerman & Zimmerman</a><br>
+			  		Initiative and funding by: <a target="_blank" href="http://www.akvo.org">Akvo</a> as part of <a target="_blank" href="http://rsr.akvo.org/project/2210/">PPP Phase 2 2011-2014</a>
+		    	</div>
 		    </div>
 		    
 		    
