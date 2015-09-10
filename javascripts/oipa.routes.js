55d4266bd1745c8d4215d993253a4a32389abb7b
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2015-09-10 23:05:08 +0200

    regression fixes

--- javascripts/oipa.routes.js
+++ javascripts/oipa.routes.js
@@ -13,7 +13,7 @@
     */
     function config($stateProvider, $locationProvider, $routeProvider, templateBaseUrl){
 
-      $locationProvider.html5Mode(false);
+      $locationProvider.html5Mode(true);
       $routeProvider.otherwise('/');
 
       $stateProvider
