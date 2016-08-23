980d1000f189d9a5fb761866910caf522cf85f1e
Daan <daan@zimmermanzimmerman.nl>
2016-08-23 13:29:38 +0200

    added 'live' search & prep for new version of oipa

--- javascripts/_partials/search/controller.search.js
+++ javascripts/_partials/search/controller.search.js
@@ -90,35 +90,35 @@
       vm.showResults = true;
 
       // get results from activities
-      Activities.searchList('&q_fields=iati_identifier,title,description&q='+vm.searchString).then(function(data, status, headers, config){
+      Activities.searchList('&q_fields=iati_identifier,title,description&q_lookup=startswith&q='+vm.searchString).then(function(data, status, headers, config){
         vm.searchData.activities.data = data.data.results;
         vm.searchData.activities.total = data.data.count;
         vm.searchData.activities.loaded = true;
       }, errorFn);
 
       // get results from countries aggregation
-      Aggregations.aggregation('recipient_country', 'count', '&q_fields=recipient_country&q=' + vm.searchString, '-count', 3).then(function(data, status, headers, config){
+      Aggregations.aggregation('recipient_country', 'count', '&q_fields=recipient_country&q_lookup=startswith&q=' + vm.searchString, '-count', 3).then(function(data, status, headers, config){
         vm.searchData.countries.data = data.data.results;
         vm.searchData.countries.total = data.data.count;
         vm.searchData.countries.loaded = true;
       }, errorFn);
 
       // get results from regions aggregation
-      Aggregations.aggregation('recipient_region', 'count', '&q_fields=recipient_region&q=' + vm.searchString, '-count', 3).then(function(data, status, headers, config){
+      Aggregations.aggregation('recipient_region', 'count', '&q_fields=recipient_region&q_lookup=startswith&q=' + vm.searchString, '-count', 3).then(function(data, status, headers, config){
         vm.searchData.regions.data = data.data.results;
         vm.searchData.regions.total = data.data.count;
         vm.searchData.regions.loaded = true;
       }, errorFn);
 
       // get results from sectors aggregation
-      Aggregations.aggregation('sector', 'count', '&q_fields=sector&q=' + vm.searchString, '-count', 3).then(function(data, status, headers, config){
+      Aggregations.aggregation('sector', 'count', '&q_fields=sector&q_lookup=startswith&q=' + vm.searchString, '-count', 3).then(function(data, status, headers, config){
         vm.searchData.sectors.data = data.data.results;
         vm.searchData.sectors.total = data.data.count;
         vm.searchData.sectors.loaded = true;
       }, errorFn);
 
       // get results from organisations aggregation
-      TransactionAggregations.aggregation('receiver_org', 'activity_count', '&q_fields=participating_org&q=' + vm.searchString, '-activity_count', 3).then(function(data, status, headers, config){
+      TransactionAggregations.aggregation('receiver_org', 'activity_count', '&q_fields=participating_org&q_lookup=startswith&q=' + vm.searchString, '-activity_count', 3).then(function(data, status, headers, config){
         vm.searchData.organisations.data = data.data.results;
         vm.searchData.organisations.total = data.data.count;
         vm.searchData.organisations.loaded = true;
