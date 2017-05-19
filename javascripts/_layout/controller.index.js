b66a8036291907f494f4e0ad01bddb74aa8919cc
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2017-05-19 12:38:53 +0200

    show 2017 data on homepage

--- javascripts/_layout/controller.index.js
+++ javascripts/_layout/controller.index.js
@@ -59,7 +59,7 @@
     vm.lineChartOptions = {};
     vm.shownIds = '';
     vm.activityStatus = '2';
-    vm.transactionYear = '2016';
+    vm.transactionYear = '2017';
 
     vm.refreshTableChart = false;
     vm.top5Options = {
