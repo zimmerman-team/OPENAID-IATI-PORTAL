012e9a26840a5061fce9e1d5554678f2bb8163a9
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-05-06 16:53:00 +0300

    Remove IATI studio referece

--- index.php
+++ index.php
@@ -12,11 +12,4 @@
   </span>
 </a>
 
-<div class="container link">
-	<a id="to-iati-studio" href="https://www.iatistudio.com/" target="_blank">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/is_logo_full.svg" title="IATI Studio">
-		<span class="hideMe">Build your own charts</span>
-	</a>
-</div>
-
 <?php get_footer(); ?>
